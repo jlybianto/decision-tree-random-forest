c7ee722e8178e5fa837145817d4f344ed4037014
jlybianto <jesse.lybianto@gmail.com>
2016-02-09 17:52:13 +0700

    Initial commit

---
+++ random_forest.py
@@ -0,0 +1,9 @@
+# ----------------
+# IMPORT PACKAGES
+# ----------------
+
+import pandas as pd
+import randomforests as rf
+from sklearn.ensemble import RandomForestClassifier
+import sklearn.metrics as skm
+import sklearn.ensemble as sk
