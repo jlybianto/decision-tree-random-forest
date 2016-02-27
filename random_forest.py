75eba465b33531375e8424d30b9ab4b0c06d5b2a
jlybianto <jesse.lybianto@gmail.com>
2016-02-26 20:31:28 -0800

    Added new headings for better clarity and profile initial data

--- random_forest.py
+++ random_forest.py
@@ -3,7 +3,23 @@
 # ----------------
 
 import pandas as pd
-import randomforests as rf
 from sklearn.ensemble import RandomForestClassifier
 import sklearn.metrics as skm
-import sklearn.ensemble as sk
+
+# ----------------
+# OBTAIN DATA
+# ----------------
+
+# Data Source: https://archive.ics.uci.edu/ml/machine-learning-databases/00240/
+
+# ----------------
+# PROFILE DATA
+# ----------------
+
+# Determine number of observations or data points in the training data set.
+subjects = pd.read_csv("uci-har-dataset/train/subject_train.txt", header=None, delim_whitespace=True, index_col=False)
+observations = len(subjects)
+participants = len(subjects.stack().value_counts())
+subjects.columns = ["Subject"]
+print("Number of Observations: " + str(observations))
+print("Number of Participants: " + str(participants))
