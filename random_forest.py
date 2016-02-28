3b73439e27686078cc7ecc219c4427330c79e968
jlybianto <jesse.lybianto@gmail.com>
2016-02-28 10:16:25 -0800

    Initiate profiling of data and data munging

--- random_forest.py
+++ random_forest.py
@@ -23,3 +23,13 @@ participants = len(subjects.stack().value_counts())
 subjects.columns = ["Subject"]
 print("Number of Observations: " + str(observations))
 print("Number of Participants: " + str(participants))
+
+# Determine the number of features in the data set.
+features = pd.read_csv("uci-har-dataset/features.txt", header=None, delim_whitespace=True, index_col=False)
+num_features = len(features)
+print("Number of Features: " + str(num_features))
+print("")
+
+# Data munging of the predictor and target variables.
+x = pd.read_csv("uci-har-dataset/train/X_train.txt", header=None, delim_whitespace=True, index_col=False)
+y = pd.read_csv("uci-har-dataset/train/y_train.txt", header=None, delim_whitespace=True, index_col=False)
